fb3e2d1c2b09db9e4225c89468147475a46bb480
BoWang <ralwayne@163.com>
2017-02-15 12:13:05 +0800

    add iqkeyboard

--- Grit/Grit/Application/AppDelegate.m
+++ Grit/Grit/Application/AppDelegate.m
@@ -9,6 +9,8 @@
 #import "AppDelegate.h"
 #import "RTNavigationController.h"
 #import "HabitModel.h"
+#import "IQKeyboardManager.h"
+
 @interface AppDelegate ()
 
 @end
@@ -23,7 +25,14 @@
     [_window setRootViewController:[RTNavigationController sharedSingleton]];
     return YES;
 }
-
+-(void)configureDependencies
+{
+    //MARK:IQKeyboard
+    [[IQKeyboardManager sharedManager]setShouldShowTextFieldPlaceholder:NO];
+    [[IQKeyboardManager sharedManager]setShouldResignOnTouchOutside:YES];
+    [[IQKeyboardManager sharedManager]setShouldToolbarUsesTextFieldTintColor:YES];
+    [[IQKeyboardManager sharedManager]setKeyboardDistanceFromTextField:80];
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- Grit/Grit/UI/Home/CreateViewController.m
+++ Grit/Grit/UI/Home/CreateViewController.m
@@ -15,13 +15,43 @@
     UITextField *_titleField;
     UITextView *_descTextView;
     UITextField *_targetField;
+    HabitType _type;
 }
 @end
 
 @implementation CreateViewController
-
+#pragma mark - Life Cycle
+- (instancetype)initWithType:(HabitType )type callBack:(CompletionBlock)callBack{
+    self = [super init];
+    _compBlock=callBack;
+    _type=type;
+    return self;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    NSString *title=nil;
+    NSString *statusStr=fast_local(@"Add");
+    switch (_type) {
+        case HabitTypeNonlinear:{
+            title=[NSString stringWithFormat:@"%@ - %@",fast_local(@"item1"),statusStr];
+        }
+            break;
+        case HabitTypeLinear:{
+            title=[NSString stringWithFormat:@"%@ - %@",fast_local(@"item2"),statusStr];
+        }
+            break;
+        default:
+            break;
+    }
+    UILabel *titleLabel=[[UILabel alloc]initWithFrame:CGRectMake(0, 0, 60, 30)];
+    [titleLabel setTextAlignment:NSTextAlignmentCenter];
+    [titleLabel setTextColor:kColor_Theme_Green];
+    [titleLabel setText:title];
+    [titleLabel setFont:fast_font_semibold(20)];
+    [self.navigationItem setTitleView:titleLabel];
+    
+    
     // Do any additional setup after loading the view.
 }
 
